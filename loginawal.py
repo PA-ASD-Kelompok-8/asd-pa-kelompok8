5dfffdbca16dbcfac3abaa92c4e40ae49ad4da64
alisyanisrinaaa <122090377+alisyanisrinaaa@users.noreply.github.com>
2023-04-12 21:50:09 +0800

    Update loginawal.py

--- loginawal.py
+++ loginawal.py
@@ -1,9 +1,51 @@
 import os
 import pwinput
 import json
+import mysql.connector
+
+mydb = mysql.connector.connect(
+    host="sql12.freemysqlhosting.net",
+    user="sql12611967",
+    password="dbopan7",
+    database= "sql12611967"
+    )
+
+mycursor = mydb.cursor()
+
+def tambahtabel():
+    global x
+    while True:
+        x = str(input("Masukkan Nama Tabel History : "))
+        if x == "":
+            print("<<< Nama Tabel Tidak Boleh Kosong !!! >>>")
+        else:
+            mycursor.execute("CREATE TABLE"+" "+x+" "+"(status VARCHAR(20), keterangan VARCHAR(50), waktu VARCHAR(30))")
+            break
+
+def indextabel():
+    mycursor.execute("show tables")
+    indexnama = x
+    tampung = []
+    for row in mycursor:
+        for i in row:
+            tampung.append(i)
+    if indexnama in tampung:
+        return(tampung.index(indexnama))
+    else:
+        print("<<< Nama Tabel Tidak Ada Di Database >>>")
+
+def namatabel(index):
+    mycursor.execute("show tables")
+    tampung = []
+    for row in mycursor:
+        for i in row:
+            tampung.append(i)
+    for i in range(len(tampung)):
+        if i == index:
+            return(tampung[index])
 
 admin = {"username" : ["novan", "lisya", "faiz"],
-         "password" : ["admin1", "admin2", "admin3"]}
+         "password" : ["awd", "admin2", "admin3"]}
 
 file_json = open("dataUser.json")
 data_1 = json.loads(file_json.read())
@@ -12,6 +54,7 @@ def settingUser():
 		json.dump(data_1, dataBaru)
 		
 def login_user():
+    global tabelhistory
     while True:
         username = input("Silahkan Masukkan Username Anda : ")
         password = pwinput.pwinput ("Silahkan Masukkan Password Anda : ")
@@ -19,12 +62,13 @@ def login_user():
             try:
                 login = data_1["username"].index(username)
                 if username == data_1["username"][login] and password == data_1["password"][login]:
+                    tabelhistory = str(namatabel(login))
                     os.system('cls')
-                    break
+                    return tabelhistory
                 else:
                     print("\n <<< Password Anda Salah >>> \nSilahkan Coba Kembali\n")
             except ValueError:
-                print("\n<<< Username Anda Salah>>> \nSilahkan Coba Kembali\n")
+                print("\n<<< Username Anda Salah >>> \nSilahkan Coba Kembali\n")
 
 def tambah_user():
     while True:
@@ -33,14 +77,15 @@ def tambah_user():
             if username.isalnum()==False:
                 print("<<< Username tidak boleh ada spasi, simbol dan kosong >>>")
             elif username in data_1["username"]:
-                print("<<< Username telah digunakan >>>")
+                print("<<< Maaf, Username telah digunakan >>>")
             elif username not in data_1["username"]:
-                password=str(pwinput.pwinput("Masukkan password : ")).replace("\t","").replace(" ","")
+                password=str(pwinput.pwinput("Masukkan Password : ")).replace("\t","").replace(" ","")
                 if password =="":
                     print("!!! Password dilarang kosong dan menggunakan spasi !!! ")
                 else:
-                    data_1["username"].append(username)
-                    data_1["password"].append(password)
+                    tambahtabel()
+                    data_1["username"].insert(indextabel(),username)
+                    data_1["password"].insert(indextabel(),password)
                     os.system('cls')
                     print("Registrasi berhasil ^____^")
                     settingUser()
