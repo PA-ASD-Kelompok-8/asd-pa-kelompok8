2b9f8b2c626d8a4a7e5dd97d6929ada184c203c1
Awang Muhammad Novandra Arissaputra <122107967+Novandra7@users.noreply.github.com>
2023-04-29 22:15:40 +0800

    Add files via upload

---
+++ koneksi.py
@@ -0,0 +1,17 @@
+import mysql.connector
+from colorama import Fore,Style
+
+def database():
+    try:
+        mydb = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            password="",
+            database = "asd")
+        if mydb.is_connected():
+            return mydb
+    except Exception as error:
+        if error:
+            print(Fore.RED + f'Error:\n{error}.',Style.RESET_ALL)
+            print(Fore.GREEN+"Harap nyalakan Apache dan MySQL di XAMPP dahulu",Style.RESET_ALL)
+            exit()
